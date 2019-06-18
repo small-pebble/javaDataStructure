3f2c45d1a6d206ee86d3cd1f0770de1e4336ec58
dyy <dyy_0805@126.com>
2019-06-18 23:39:36 +0800

    implments avl

--- src/com/atguigu/datastructures/avl/AVLTreeDemo.java
+++ src/com/atguigu/datastructures/avl/AVLTreeDemo.java
@@ -1,42 +1,37 @@
-package com.atguigu.datastructures.binarysorttree;
+package com.atguigu.datastructures.avl;
+
 
 /**
  * @Auther:smallPebble
  * @Date:2019/6/17
- * @Description:com.atguigu.datastructures.binarysorttree
+ * @Description:com.atguigu.datastructures.avl
  * @version:1.0
  **/
-public class BinarySortTreeDemo {
+public class AVLTreeDemo {
     public static void main(String[] args) {
-        int[] arr = {7, 3, 10, 12, 5, 1, 9};
-        BinarySortTree bst = new BinarySortTree();
+        //int[] arr = {4,3,6,5,7,8};
+        //int[] arr = {10,12,8,9,7,6};
+        int[] arr = {10,11,7,6,8,9};
+        //int[] arr = {1,2,3};
+        AVLTree avl = new AVLTree();
         for (int i : arr) {
             Node node = new Node(i);
-            bst.add(node);
+            avl.add(node);
         }
-
-        bst.delete(2);
-        bst.delete(5);
-        bst.delete(9);
-        bst.delete(12);
-        bst.delete(7);
-        bst.delete(3);
-        bst.delete(10);
-        bst.delete(1);
-        /*bst.delete(12);
-        bst.delete(10);*/
-       /* bst.delete(1);
-        bst.delete(3);
-        bst.delete(5);
-        bst.delete(7);
-        bst.delete(12);*/
-
-        bst.infixOrder();
+        avl.infixOrder();
+        System.out.println(avl.root.height());
+        System.out.println(avl.root.leftHeight());
+        System.out.println(avl.root.rightHeight());
+        System.out.println(avl.root.right.left);
     }
 }
 
-class BinarySortTree {
+class AVLTree {
     Node root;
+
+    public Node getRoot() {
+        return root;
+    }
 
     //删除以node为根节点的最小值，并返回这个值
     public int delRightTreeMin(Node node){
@@ -150,6 +145,44 @@ class Node {
     public Node(int value) {
         this.value = value;
     }
+    //左子树的高度
+    public int leftHeight(){
+        if(left==null){
+            return 0;
+        }
+        return this.left.height();
+    }
+    //右子树的高度
+    public int rightHeight(){
+        if (right==null){
+            return 0;
+        }
+        return this.right.height();
+    }
+
+    public int height(){
+        return Math.max(left==null?0:this.left.height(),right==null?0:this.right.height())+1;
+    }
+
+    private void leftRotate(){
+        //需要保证根节点不变，按以下步骤
+        //创建一个与该节点相同的新节点
+        Node newNode = new Node(this.value);
+        newNode.left = this.left;
+        this.left = newNode;
+        newNode.right = this.right.left;
+        this.value = this.right.value;
+        this.right = this.right.right;
+    }
+
+    private void rightRotate(){
+        Node newNode = new Node(this.value);
+        newNode.right = this.right;
+        newNode.left = this.left.right;
+        this.right = newNode;
+        this.value = this.left.value;
+        this.left = this.left.left;
+    }
 
     //查找要删除的节点
     public Node searchNode(int value) {
@@ -205,6 +238,23 @@ class Node {
             } else {
                 this.right.add(node);
             }
+        }
+        //进行左旋转
+        if(rightHeight()-leftHeight()>1){
+            //如果右子树的左子树高度大于右子树的右子树
+            if(right.leftHeight()>right.rightHeight()){
+                right.rightRotate();
+            }
+            leftRotate();
+            return;
+        }
+        //进行右旋转
+        if(leftHeight()-rightHeight()>1){
+            //如果左子树的右子树高度大于左子树的左子树
+            if(left.rightHeight()>left.leftHeight()){
+                left.leftRotate();
+            }
+            rightRotate();
         }
     }
 
